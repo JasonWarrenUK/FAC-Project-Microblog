4cc3c62260703b611d26a005baa3196a0a77a0a0
Jason Warren <contact.jwarren@gmail.com>
2024-04-19 17:15:34 +0100

    copy files from previous challenge

---
+++ server.js
@@ -0,0 +1,59 @@
+/* ----- SETUP ----- */
+const express = require("express");
+const server = express();
+const staticHandler = express.static("public");
+// const bodyParser = express.urlencoded();
+
+const { home } = require("./templates.js");
+
+const { valid, validReset } = require("./utils.js");
+
+function logger(req, res, next) {
+  console.log(`${req.method} ${req.url}`);
+	next();
+}
+
+server.use(staticHandler);
+server.use(logger);
+
+/* ----- POSTS ----- */
+const posts = [];
+
+server.get("/", (req, res) => {
+  const body = home(posts);
+  res.send(body);
+});
+
+server.post("/", express.urlencoded({ extended: false }), (req, res) => {
+	validReset();
+
+	const nickname = req.body.nickname;
+	if (nickname == "") {
+		valid.name = false;
+		valid.nameWarn = `<p>please enter your nickname</p>`;
+		valid.all = false;
+	}
+
+  const message = req.body.message;
+	if (message == "") {
+		valid.message = false;
+		valid.messageWarn = `<p>please enter a message</p>`;
+		valid.all = false;
+	}
+
+  const created = Date.now();
+
+	if (valid.all) {
+		console.log(`Valid Post`);
+		posts.push({ nickname, message, created });
+		res.redirect("/");
+	} else {
+		console.log(`Invalid Post`);
+		// res.status(400).redirect("/");
+		const body = home(posts, req.body);
+		res.status(400).send(body);
+	}
+});
+
+/* ----- EXPORTS ----- */
+module.exports = server;
---
+++ utils.js
@@ -0,0 +1,62 @@
+function sanitize(dirty) {
+	// console.groupCollapsed(`Running Sanitize`);
+	// console.log(`Input: ${dirty}`);
+
+	// let caught = 0;
+
+	// TODO add `>&'"` back into below
+	const clean = dirty.replace(/[<]/g,
+		function(match) {
+			switch (match) {
+				case '<':
+					// console.log(`Found <`);
+					// caught++;
+					return '&lt;';
+				// case '>':
+				// 	console.log(`Found >`);
+				// 	caught++;
+				// 	return '&gt;';
+				// case '&':
+				// 	console.log(`Found &`);
+				// 	caught++;
+				// 	return '&amp;';
+				// case "'":
+				// 	console.log(`Found '`);
+				// 	caught++;
+				// 	return '&#39;';
+				// case '"':
+				// 	console.log(`Found "`);
+				// 	caught++;
+				// 	return '&quot;';
+			}
+		}
+	);
+
+	// if (caught == 0) { console.log(`No Characters Caught`) }
+
+	// console.log(`Output: ${clean}`);
+	// console.groupEnd();
+	
+	return clean;
+}
+
+let valid = {
+	name: true,
+	nameWarn: ``,
+	message: true,
+	messageWarn: ``,
+	all: true
+};
+
+function validReset() {
+	valid.name = true;
+	valid.nameWarn = ``;
+	valid.message = true;
+	valid.messageWarn = ``;
+	valid.all = true;
+}
+
+module.exports = {
+	sanitize,
+	valid, validReset
+};
